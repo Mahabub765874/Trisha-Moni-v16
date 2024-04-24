2c84eca18330727843ee814d6b0e5f51d7b0417c
Santa7658 <167959973+Santa7658@users.noreply.github.com>
2024-04-25 03:05:27 +0600

    Create 0korgus.js

---
+++ scripts/commands/0korgus.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+module.exports.config = {
+	name: "korgus.mp3",
+    version: "1.1.1",
+prefix: true,
+	permssion: 0,
+	credits: "John Lester", 
+	description: "Just Respond",
+	category: "no prefix",
+    cooldowns: 10, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("🐇")==0 || event.body.indexOf("🐰")==0 || event.body.indexOf("🐇")==0 || event.body.indexOf("🐰")==0) {
+		var msg = {
+				body: "-এই খরগোশ গাজর খাবি নাকি তুই-!!😋",
+				attachment: fs.createReadStream(__dirname + `/cache/korgus.mp3`)
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+                                        }
